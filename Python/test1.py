efa4be806bff61f369069aaf019d2fbf2e48c0e6
Asterisk-developer <asteriskdeveloper.dev@gmail.com>
2023-01-17 21:04:37 +0530

    17012023

--- Python/test1.py
+++ Python/test1.py
@@ -2,14 +2,33 @@ import csv
 from itertools import groupby
 from operator import itemgetter
 
-with open('testcsv.csv','r') as csv_file:    
+print('hello')
+items =[{}]
+
+with open(r'new/testcsv.csv','r') as csv_file:    
     csv_reader = csv.DictReader(csv_file)
 
     # for line in csv_reader:
     #     print(line)
-    items = sorted(csv_reader,key=itemgetter('Position'))
+    items = sorted(csv_reader,key=itemgetter('CITY-NAME'))
+    # print('hello',items)
 
-    for key, value in groupby(items,key=itemgetter('Position')):
-        print(key)
-        for k in value:
-            print(k['Name'])
+    # for key, value in groupby(items,key=itemgetter('CITY-NAME')):
+    #     print(key)
+    #     for k in value:
+    #         print(k)
+
+fields = ['MIG-ID','SHIP-ID','SITE-NAME','CITY-NAME']
+
+filename = "university_records.csv"
+    
+# writing to csv file 
+with open(filename, 'w') as csvfile: 
+    # creating a csv dict writer object 
+    writer = csv.DictWriter(csvfile, fieldnames = fields) 
+        
+    # writing headers (field names) 
+    writer.writeheader() 
+        
+    # writing data rows 
+    writer.writerows(items) 
